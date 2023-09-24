a9013dcede1841fa464a865101d5cf1fb56d5727
dtta222 <david.t.taylor222@gmail.com>
2023-09-24 01:14:52 -0400

    complete objective2lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,12 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Charles Peterson";
+    int age = 47;
+    String state = "Arkansas";
+
+    /*
+      Your solution goes here
+    */
+    System.out.println("Name: " + name + " Age: " + age + " State: " + state);
+  }
+}
