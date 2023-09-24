7cc5066a5b4bd3327168d8aa71b626dae221f28f
dtta222 <david.t.taylor222@gmail.com>
2023-09-24 00:32:25 -0400

    complete objective1lab2

---
+++ Labs/Objective1Lab2.java
@@ -0,0 +1,24 @@
+public class Objective1Lab2 {
+  public static void main(String[] args) {
+    /*
+      Your solution goes here
+    */
+    /*
+  ----------------
+ | Grocery List   |
+ | 1. Milk        |
+ | 2. Eggs        |
+ | 3. Bread       |
+  ----------------
+  */
+    System.out.println(" ----------------\n| Grocery List   |\n| 1. Milk        |\n| 2. Eggs        |\n| 3. Bread       |\n ----------------");
+    
+  }
+}
+
+
+
+
+
+
+
