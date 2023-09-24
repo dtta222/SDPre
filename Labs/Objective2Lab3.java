05741f88046100da623d954ea10c764287adb2b5
dtta222 <david.t.taylor222@gmail.com>
2023-09-24 01:23:10 -0400

    complete Objective2Lab3

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,14 @@
+public class Objective2Lab3 {
+  public static void main(String[] args) {
+    /*
+      Your solution goes here
+    */
+    int year = 1996;
+    String month = "June";
+    boolean weekend = false;
+
+    System.out.println(year);
+    System.out.println(month);
+    System.out.println(weekend);
+  }
+}
