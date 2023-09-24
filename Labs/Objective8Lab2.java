c7570a9229e44e3b17979d000532ce9b4ca57107
dtta222 <david.t.taylor222@gmail.com>
2023-09-24 13:50:38 -0400

    complete Objective8Lab2

---
+++ Labs/Objective8Lab2.java
@@ -0,0 +1,10 @@
+public class Objective8Lab2 {
+  public static void main(String[] args) {
+    //TODO Using a for loop, write a program that sums up the numbers 1 to 20
+    int sum = 0;
+    for(int i = 1; i <= 20; i++) {
+      sum += i;
+    }
+    System.out.println(sum);
+  }
+}
