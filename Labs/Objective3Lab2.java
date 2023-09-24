c4722498f54c8b3d542a12e52c3664a03fbb78ff
dtta222 <david.t.taylor222@gmail.com>
2023-09-24 02:00:51 -0400

    complete Objective3Lab2

---
+++ Labs/Objective3Lab2.java
@@ -0,0 +1,7 @@
+public class Objective3Lab2 {
+    public static void main(String[] args) {
+        double side1 = 10, side2 = 8, hypotenuse;
+        double result = (side1 * side1) + (side2 * side2);
+        System.out.println(Math.sqrt(result));
+    }
+}
