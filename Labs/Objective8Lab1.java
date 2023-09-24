5f2381424aa65ae7c2b9dca4c4cd835e766ef2ef
dtta222 <david.t.taylor222@gmail.com>
2023-09-24 12:58:20 -0400

    complete Objective8Lab1

---
+++ Labs/Objective8Lab1.java
@@ -0,0 +1,18 @@
+public class Objective8Lab1 {
+  public static void main(String[] args) {
+    //TODO Write a for loop that will print the numbers 1 to 10
+    int counter = 1;
+    System.out.println(counter);
+    while(counter < 10) {
+      counter++;
+      System.out.println(counter);
+    }
+
+    //TODO Write another for loop that will print the numbers 10 to 1.
+    System.out.println(counter);
+    while(counter > 1) {
+      counter--;
+      System.out.println(counter);
+    }
+  }
+}
